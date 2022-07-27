d1f9f77736f5fcd21097008e9bf824f8e030629e
Shreeraj-Khatiwada <51294273+Shreeraj-Khatiwada@users.noreply.github.com>
2022-07-27 16:20:51 +0545

    Update ProductRepository.java

--- src/main/java/com/shreerajtech/crud/example/repository/ProductRepository.java
+++ src/main/java/com/shreerajtech/crud/example/repository/ProductRepository.java
@@ -2,7 +2,7 @@ package com.shreerajtech.crud.example.repository;
 
 import com.shreerajtech.crud.example.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
 
